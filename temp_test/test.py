3675fc205325de3a63541401ff05d0152bd198aa
chenpeng <ex-chenp003@cmft.com>
2022-06-19 17:22:50 +0800

    [modify]: 修改test文件

--- temp_test/test.py
+++ temp_test/test.py
@@ -2,19 +2,19 @@
 # @Time    : 2021/7/21 11:01
 # @Author  : chenp
 # @File    : test.py
-# class CLanguage():
-#
-#     def __init__(self):
-#         hobbit = "eat"
-#         self.name = "test XX"
-#     # 定义__call__方法
-#     def __call__(self, name, add):
-#         print("调用__call__()方法", name, add)
-#         print(self.name)
-#     print("test YY")
-#
-#
-# clangs = CLanguage()
-# # clangs("hello, word", "test")
-# clangs.__call__("hello", "world")
+class CLanguage():
+
+    def __init__(self):
+        hobbit = "eat"
+        self.name = "test XX"
+    # 定义__call__方法
+    def __call__(self, name, add):
+        print("调用__call__()方法", name, add)
+        print(self.name)
+    print("test YY")
+
+
+clangs = CLanguage()
+# clangs("hello, word", "test")
+clangs.__call__("hello", "world")
 
